7bdb2540fbd5734d54d9ad15f12a78563adace81
Elina Razina <elina.razina@dataart.com>
2020-08-21 17:32:44 +0300

    etsy footerlink and classnames

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,9 @@ import vk from './../../images/vk.svg';
 import etsy from './../../images/etsy.svg';
 import email from './../../images/email.svg';
 import {Link} from 'react-router-dom';
+import classNames from 'classnames';
 
+let etsyFooterLink = classNames(`${styles.footerlink}`, `${styles.etsy}`);
 
 function Footer() {
   return (
@@ -17,7 +19,7 @@ function Footer() {
             <a href="https://www.facebook.com/Sl.Fineart85/" target="_blank"><img  className={styles.footerlink} src={facebook} alt="Svetlana Lebedeva's facebook"></img></a>
             <a href="https://wa.me/79045126424/?text=Type%20your%20message%20here" target="_blank"><img className={styles.footerlink}  src={watsapp} alt="write a message or call Svetlana Lebedeva by watsapp"></img></a>
             <a href="https://vk.com/slebedeva8" target="_blank"><img src={vk} className={styles.footerlink}  alt="Svetlana Lebedeva's vkontakte profile - russian social network"></img></a>
-            <a href="https://www.etsy.com/people/SVento" target="_blank"><img src={etsy} className={styles.footerlink}  alt="watch all Svetlana Lebedeva's art works on etsy.com"></img></a>
+            <a href="https://www.etsy.com/people/SVento" target="_blank"><img src={etsyFooterLink} className={i} alt="watch all Svetlana Lebedeva's art works on etsy.com"></img></a>
             <a href="mailto:s.vento85@gmail.com" target="_blank"><img className={styles.footerlink}  src={email} alt="email to Svetlana Lebedeva"></img></a>
         </div>
         <div>Developed by <a className={styles.link} href="http://www.erazina.info">Elina</a>, designed by <Link to="/"className={styles.link}>Sl•</Link></div>
